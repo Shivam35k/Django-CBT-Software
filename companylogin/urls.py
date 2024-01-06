93bf7433d4b3a267d1c6a1a69a69ea8a9dbe8d61
Shivam35k <114252890+Shivam35k@users.noreply.github.com>
2024-01-06 14:47:15 +0530

    Final commit

---
+++ companylogin/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from companylogin import views
+
+urlpatterns=[
+    path('com_dashboard/',views.com_dashboard,name="com_dashboard"),
+    path('addexam/',views.addExam,name="addexam"),
+    path('ongoingtests/',views.ongoing_tests,name="ongoingtests"),
+    path('completedtests/',views.completed_tests,name="completedtests"),
+    path('centers/',views.centers,name="centers"),
+    path('addcenter/',views.addCenter,name="addcenter"),
+    path('saveexam/',views.saveExam,name="saveexam"),
+    path('logout/',views.comLogout,name="logout"),
+    path('company-entry/',views.companyentry,name="companyentry"),
+    path('password-center/',views.passcenter,name="passcenter"),
+    path('questions/',views.questions,name="questions"),
+    path('addquestion/',views.addQuestion,name="addquestion"),
+    
+]
